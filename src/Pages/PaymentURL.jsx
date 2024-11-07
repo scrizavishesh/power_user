2f3ba79464c012266e2da7bf3fb79123ae61e1b7
Vishesh Tomer <vishesh@scriza.in>
2024-11-07 11:20:30 +0530

    Your commie

--- src/Pages/PaymentURL.jsx
+++ src/Pages/PaymentURL.jsx
@@ -26,7 +26,7 @@ const PaymentURl = () => {
             }
         } catch (err) {
             console.log(err);
-            // alert(err?.response?.data?.error);
+            alert(err?.response?.data?.error);
         }
     };
 
